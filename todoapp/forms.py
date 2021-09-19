2fb65e00d71fc2ee0a2bd6177e5c06e35d3a25fc
zfelipesilveira <zfelipesilveira@gmail.com>
2021-09-19 19:10:47 -0300

    arrumando o forms.py

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Mytodo
 
 class TodoForm(forms.ModelForm):
+    task = forms.CharField(max_length = 50, widget=forms.TextInput(attrs = {
+        'id': 'todoField', 'placeholder': 'Enter Task'
+    }))
     class Meta:
         model = Mytodo
-        fields = [ 'task', ]
+        fields = ['task',]
